e02e063f4f3d04ae23447f9797aed59d772871a5
damasterang <damasterang@outlook.com>
2014-05-30 14:42:06 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+	$server = "localhost";
+	$username = "root";
+	$password = "abcd";
+	$database = "jst";
+
+	mysql_connect($server, $username, $password);
+	mysql_select_db($database);
+
+?>
